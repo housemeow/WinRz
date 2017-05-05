a620e85b939818ec77133c6258753b402a778a16
housemeow <housemeow@gmail.com>
2017-05-05 11:01:38 +0800

    change icon

--- WndRzForm.Designer.cs
+++ WndRzForm.Designer.cs
@@ -43,7 +43,7 @@
             this.notifyIcon1.BalloonTipTitle = "title";
             this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip2;
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Text = "WndRz";
             this.notifyIcon1.Visible = true;
             // 
             // contextMenuStrip2
